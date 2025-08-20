d7a8253530ef083d7b232e854234fb4712bc02b3
harrylam0016 <harrylam0016@gmail.com>
2025-08-20 07:26:29 +0700

    Feat (Player): Add collision handling between player and enemy

---
+++ hardest-game-demo/Assets/Scripts/Player/PlayerCollision.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerCollision : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            SceneManager.LoadScene("StartScene");
+        }
+    }
+}
