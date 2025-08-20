f2a38e23f381cba544e8a607bec88c8b2941b8e6
harrylam0016 <harrylam0016@gmail.com>
2025-08-20 07:10:31 +0700

    Feat (Goal): Add goal trigger that loads next scene

---
+++ hardest-game-demo/Assets/Scripts/GoalTrigger.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoalTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            int nextSceneIndex = currentSceneIndex + 1;
+
+            if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+            {
+                SceneManager.LoadScene(nextSceneIndex);
+            }
+            else
+            {
+                SceneManager.LoadScene(0);
+            }
+        }
+    }
+}
